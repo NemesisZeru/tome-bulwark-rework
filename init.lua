63c656937e7c0bfe068c315b91aa481c7bb5bfc5
NemesisZeru <Zeru65x@aol.com>
2014-11-03 11:20:40 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- Elementals Race, for Tales of Maj'Eyal.
+-- Bulwark Update, for Tales of Maj'Eyal.
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
