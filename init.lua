9508a882fd1acbefe190c654a61d76f98e026741
NemesisZeru <Zeru65x@aol.com>
2014-11-03 11:19:24 -0500

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,28 @@
+-- Elementals Race, for Tales of Maj'Eyal.
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+long_name = 'Bulwark Update!'
+short_name = 'bulwark-rework'
+for_module = 'tome'
+version = {1,2,4,}
+weight = 100
+author = {'NemesisZeru', 'StarKeep', 'Razakai} -- More to be added, probably.
+description = 'Some fun new toys for the Bulwarks to play with!'
+tags = {'class', 'bulwark', 'shields', 'update', 'rework'}
+
+overload = true
+superload = true
+hooks = true
+data = true
